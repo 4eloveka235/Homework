38a8f1586d48adfb238cc9acbb973537e2416791
4eloveka235 <152286375+4eloveka235@users.noreply.github.com>
2024-11-01 19:33:13 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = (input('Здорова, напиши как тя звать-то хоть (напиши имя и фамилию через пробел): ')) #прошу пробел, так как надо будет потом по заданию :)
+print('Количество введённых символов: ' + str(len(my_string)))
+
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(' ',''))
+
+print(my_string[0])
+print(my_string[-1])
